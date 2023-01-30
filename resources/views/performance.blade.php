645b28b3cb3d16af231058d680c43e37fba0123c
younghiro <younghiromu112010@icloud.com>
2023-01-30 15:50:21 +0100

    html file

---
+++ resources/views/performance.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSSの読み込み -->
+    <link rel="stylesheet" href="reset.css">
+    <link rel="stylesheet" href="performance.css">
+    <!-- -->
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>

8b38c8f2abb50a3cfabcd4ea51ffe0aaf3c3c535
Haruto Mori <baystarsman@outlook.jp>
2023-01-31 21:25:09 +0100

    create a blueprint of html

--- resources/views/company_profile.blade.php
+++ resources/views/company_profile.blade.php
@@ -6,11 +6,130 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- CSSの読み込み -->
     <link rel="stylesheet" href="reset.css">
-    <link rel="stylesheet" href="company_profile.css">
+    <link rel="stylesheet" href="C:\Users\bayst\coding\leadHP\resources\css\company_profile.css">
     <!-- -->
     <title>Document</title>
 </head>
-<body>
-    
-</body>
+
+<body id="top">
+    <header>
+        <!-- add header here -->
+    </header>
+
+    <div class="main column1-narrow">
+        <div class="contents">
+          <h1 class="pageTtl">会社概要<span>COMPANY</span></h1>
+          <div class="company">
+            
+            <div id="message" class="company-sec1">
+              <h2 class="company__heading">
+                <span>MESSAGE</span>
+              </h2>
+  
+              <p class="company-sec1__text">
+                <!-- describe about the company -->
+                describe what my compnay is like here
+              </p>
+  
+              <div class="company-sec1__sign">
+                <div class="sign__col">
+                  <p>job position and name here</p>
+                </div><!-- /.sign -->
+              </div>
+            </div><!-- /.messageWrap -->
+            
+            <div id="phirosophy" class="company-sec2">
+              <h2 class="company__heading company__heading_oneline">理念<span>PHILOSOPHY</span></h2>
+  
+              <div class="company-sec2__card">
+                <div class="company-sec2__row">
+                  <h3 class="company-sec2__heading">
+                    <picture>
+                      <source media="(min-width: 480px)"
+                        srcset="https://estate-ring.co.jp/cms/wp-content/themes/estatering3/images/comprofile_phirosophy_mission.png">
+                      <source media="(min-width: 321px)"
+                        srcset="https://estate-ring.co.jp/cms/wp-content/themes/estatering3/images/comprofile_phirosophy_mission_sp.png">
+                      <img src="https://estate-ring.co.jp/cms/wp-content/themes/estatering3/images/comprofile_phirosophy_mission.png" alt="ミッション">
+                    </picture>
+                  </h3>
+  
+                  <p class="company-sec2__text"> describe about company mission here</p>
+                </div>
+  
+                <div class="company-sec2__row">
+                  <h3 class="company-sec2__heading">
+                    <picture>
+                      <source media="(min-width: 480px)"
+                        srcset="https://estate-ring.co.jp/cms/wp-content/themes/estatering3/images/comprofile_phirosophy_philosophy.png">
+                      <source media="(min-width: 320px)"
+                        srcset="https://estate-ring.co.jp/cms/wp-content/themes/estatering3/images/comprofile_phirosophy_philosophy_sp.png">
+                      <img src="https://estate-ring.co.jp/cms/wp-content/themes/estatering3/images/comprofile_phirosophy_philosophy.png" alt="企業理念">
+                    </picture>
+                  </h3>
+  
+                  <p class="company-sec2__text">describe about corporate philosophy here</p>
+                </div>
+  
+                <div class="company-sec2__row">
+                  <h3 class="company-sec2__heading">
+                    <picture>
+                      <source media="(min-width: 480px)"
+                        srcset="https://estate-ring.co.jp/cms/wp-content/themes/estatering3/images/comprofile_phirosophy_promise.png">
+                      <source media="(min-width: 320px)"
+                        srcset="https://estate-ring.co.jp/cms/wp-content/themes/estatering3/images/comprofile_phirosophy_promise_sp.png">
+                      <img src="https://estate-ring.co.jp/cms/wp-content/themes/estatering3/images/comprofile_phirosophy_promise.png" alt="お約束">
+                    </picture>
+                  </h3>
+                  <ul class="company-sec2__list">
+                    <li><span>1. </span>company promise 1</li>
+                    <li><span>2. </span>company promise 2</li>
+                    <li><span>3. </span>company promise 3</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            
+  
+            <div id="profile" class="company-sec3">
+              <h2 class="company__heading company__heading_oneline">会社概要<span>PROFILE</span><h2>
+  
+              <dl class="company-sec3__list">
+                <dt>会社名</dt>
+                <dd>company name</dd>
+  
+                <dt>事業内容</dt>
+                <dd>
+                  ○<br>
+                  ○<br>
+                  ○<br>
+                  ○<br>
+                </dd>
+  
+                <dt>事業エリア</dt>
+                <dd>area</dd>
+  
+                <dt>会社所在地</dt>
+                <dd>company address</dd>
+  
+                <dt>job title</dt>
+                <dd>full name</dd>
+  
+                <dt>宅地建物取引業<br>免許証番号</dt>
+                <dd>your cetificates</dd>
+  
+                <dt>物件情報掲載サイト</dt>
+                <dd>
+                  ○<br>
+                  ○<br>
+                  ○<br>
+                  ○<br>
+                </dd>
+              </dl>
+            </div><!-- /.outlineWrap -->
+            
+            
+  
+          </div><!-- /.company -->
+        </div><!-- /.contents -->
+      </div><!-- /.main -->
 </html>
